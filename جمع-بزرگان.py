8f3068f187b01d4d463f6cad487f60b4e28c780a
Mam-Mad <75883500+Mam-Mad@users.noreply.github.com>
2021-07-25 15:51:03 +0430

    Create جمع-بزرگان.py

---
+++ جمع-بزرگان.py
@@ -0,0 +1,13 @@
+"""
+Question:
+https://quera.ir/problemset/contest/2551/سؤال-جمع-بزرگان
+"""
+
+x1 = int(input())
+b = input()
+x2 = int(input())
+
+if b == '+':
+    print(x1 + x2)
+elif b == '*':
+    print(x1 * x2)
